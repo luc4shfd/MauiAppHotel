4823cae6816ef3871d01e16c5ca8c904167da2f7
tecdevetec <lucashfdantas@gmail.com>
2024-11-03 20:47:04 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var Window = base.CreateWindow(activationState);
+
+            Window.Width = 400;
+            Window.Height = 600;
+
+
+            return Window;
+        }
+    }
+}
